2fd3683a4df1e13d3e5969df8c11787eccb6e6b2
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-10-12 19:49:51 +1100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 # Updater for LeafPy. This will run and determine if there is a new version available. This file will be added to LeafPy on start up and prompt the user to update.
 
-latestVersion = cat < latest
-computerVersion = $1
+$latestVersion = cat < ./latest
+$computerVersion = $1
 
 if $latestVersion != $computerVersion then
-  input = echo "[y,n] A new version is available. Would you like to download it?"
+  $input = echo "[y,n] A new version is available. Would you like to download it?"
   if $input == "y" then
     ./downloader.sh $latestVersion
 fi
