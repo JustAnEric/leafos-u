99bc85cc7088d73bcc0b5c629ea70c2a8d7f2812
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-10-18 23:56:36 +1100

    Update home.py

--- c/home.py
+++ c/home.py
@@ -10,17 +10,33 @@ ENDC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 
-os.system('clear')
-time.sleep(2)
-print(OKGREEN+"Home"+ENDC)
-print(f"""
-{OKBLUE}[0]{ENDC} Check for Updates
-{OKBLUE}[1]{ENDC} Turn off OS
-{OKBLUE}[2]{ENDC} Kill all Tasks
-{OKBLUE}[3]{ENDC} Settings
-{OKBLUE}[4]{ENDC} Apps
-{OKBLUE}[5]{ENDC} BIOS
-{OKBLUE}[6]{ENDC} Reboot
-""")
+OS_VERSION = "19.0.0"
+OS_BUILD = "1.2"
 
-i = input(f"{WARNING}[?]{ENDC}{OKBLUE}")
+os.system('clear')
+while True:
+  time.sleep(2)
+  print(OKGREEN+"Home"+ENDC)
+  print(f"""
+  {OKBLUE}[0]{ENDC} Check for Updates
+  {OKBLUE}[1]{ENDC} Turn off OS
+  {OKBLUE}[2]{ENDC} Kill all Tasks
+  {OKBLUE}[3]{ENDC} Settings
+  {OKBLUE}[4]{ENDC} Apps
+  {OKBLUE}[5]{ENDC} BIOS
+  {OKBLUE}[6]{ENDC} Reboot
+  """)
+
+  i = input(f"{WARNING}[?]{ENDC}{OKBLUE}")
+
+  if i == 0:
+    print("Checking for updates...")
+    updater_version = open('./os/leafos-u/latest', 'r').read(1)
+    if updater_version != OS_VERSION:
+      print(f"An update is {OKGREEN}available{ENDC}. Would you like to update {WARNING}now?{ENDC}")
+      if input(f'{WARNING}[?]{ENDC}{OKBLUE}') == "y":
+        with open("./os/leafos-u/") as f: open("./os/leafos-u/")
+
+    if updater_version == OS_VERSION:
+      print(f"No updates were {FAIL}available{ENDC}.")
+      os.system('clear')
