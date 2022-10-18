4b8805301b64f1e3d15a7ac7e9123cc754a6b62e
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-10-18 23:40:24 +1100

    Update home.py

--- c/home.py
+++ c/home.py
@@ -1,25 +1,24 @@
 import os,time
 
-class col:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKCYAN = '\033[96m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
 
 os.system('clear')
 time.sleep(2)
-print("Home")
-print("""
-[0] Check for Updates
-[1] Turn off OS
-[2] Kill all Tasks
-[3] Settings
-[4] Apps
-[5] BIOS
-[6] Reboot
+print(OKGREEN+"Home"+ENDC)
+print(f"""
+{OKBLUE}[0]{ENDC} Check for Updates
+{OKBLUE}[1]{ENDC} Turn off OS
+{OKBLUE}[2]{ENDC} Kill all Tasks
+{OKBLUE}[3]{ENDC} Settings
+{OKBLUE}[4]{ENDC} Apps
+{OKBLUE}[5]{ENDC} BIOS
+{OKBLUE}[6]{ENDC} Reboot
 """)
