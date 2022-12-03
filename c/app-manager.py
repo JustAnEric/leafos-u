bec6e23bd9bdd65cf17f17efbd432dfe98e288f8
JustAnEric <73326807+WWEMGamer2@users.noreply.github.com>
2022-12-03 17:55:42 +1100

    Update app-manager.py

--- c/app-manager.py
+++ c/app-manager.py
@@ -1,5 +1,15 @@
 import os,shutil,re,time
 
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKCYAN = '\033[96m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
 A0 = open_filemanager
 A1 = open_musicapp
 A2 = open_timerapp
